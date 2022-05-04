12e8958579f6dff981e5723cab7d52d01eb96b35
Greg Doud <gdoud@gmail.com>
2022-05-04 14:33:59 -0400

    Add project files.

---
+++ MyFirstClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using MyFirstClass;
+
+var evan = new TqlStudent("Evan", "Mercurio", 5);
+var evangit = new Assessment() {
+    Topic = "Git/GitHub", Points = 110, Taken = DateTime.Now
+};
+evan.GitGithubAssessment = evangit;
+
+evan.HappyAnniversary();
+
+Console.WriteLine(evan.Print());
+
+var greg = new TqlStudent("Greg", "Doud", 0);
+
+Console.WriteLine(greg.Print());
+
+var git = new Assessment() { Topic = "Git/GitHub", Points = 110, Taken = DateTime.Now };
+var sql = new Assessment() { Topic = "SQL", Points = 110, Taken = DateTime.Now };
+var test = new TqlStudent() {
+    Firstname = "Test", YearsOfService = 99, GitGithubAssessment = git
+};
+test.SqlAssessment = sql;
+
+Console.WriteLine(test.Print());
+
+
---
+++ MyFirstClass/TqlStudent.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstClass {
+
+    internal class TqlStudent {
+
+        private static int NextId = 1;
+
+        public int Id { get; set; } = 0;
+        public string Firstname { get; set; } = string.Empty;
+        public string Lastname { get; set; } = string.Empty;
+        public int YearsOfService { get; set; } = 0;
+        public Assessment? GitGithubAssessment { get; set; } = null;
+        public Assessment? SqlAssessment { get; set; } = null;
+        
+
+        public string Print() {
+            var msg = $"{Id}: {Fullname()} has {YearsOfService} year{(YearsOfService > 1 ? "s" : "")} of service at TQL.";
+            if(GitGithubAssessment is not null) {
+                msg = msg + $"\nAssessment {GitGithubAssessment!.Topic}, Points: {GitGithubAssessment.Points}";
+            }            
+            if(SqlAssessment is not null) {
+                msg = msg + $"\nAssessment {SqlAssessment!.Topic}, Points: {SqlAssessment.Points}";
+            }
+            return msg;
+        }
+        public string Fullname() {
+            return $"{this.Firstname} {Lastname}";
+        }
+        public void HappyAnniversary() {
+            YearsOfService++;
+        }
+
+        public TqlStudent(string firstname, string lastname, int yearsOfService) {
+            Firstname = firstname;
+            this.Lastname = lastname;
+            this.YearsOfService = yearsOfService;
+            Id = NextId++;
+        }
+        public TqlStudent() {
+            Id = NextId++;
+        }
+
+    }
+}
